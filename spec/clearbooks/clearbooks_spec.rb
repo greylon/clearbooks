bfadde5cba9357ca8852b77a0cec828284a0ff2e
Pedro Lopez <pedrolopez@gmx.de>
2015-04-16 04:25:00 -0500

    First tests

---
+++ spec/clearbooks/clearbooks_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'savon'
+
+describe Clearbooks do
+  let(:wsdl_url) { 'https://secure.clearbooks.co.uk/api/wsdl/' }
+  
+  before :all do
+    @client = Savon::Client.new wsdl_url
+  end
+
+  describe "requests respond" do
+    it "should connect successfully" do
+      pending
+    end
+  end
+
+end
