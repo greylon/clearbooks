77d5f28dd173c0129a515d7156b14e33c9f8eaf2
Oleg Kukareka <oleg@kukareka.com>
2015-04-28 23:08:27 +0300

    YARD docs on model attributes attributes

--- lib/clearbooks/model/invoice.rb
+++ lib/clearbooks/model/invoice.rb
@@ -2,14 +2,52 @@ module Clearbooks
   # @class Clearbooks Invoice model {{{
   # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
   class Invoice < Base
-    attr_reader :id, :date_created, :date_due, :credit_terms, :description, :entity_id, :reference, :project, :status, :invoice_prefix,
-                :invoice_number, :external_id, :statement_page, :date_modified, :items, :type
+    attr_reader :date_created, :date_due, :credit_terms, :description, :entity_id, :reference, :project, :status, :invoice_prefix,
+                :invoice_number, :external_id, :statement_page, :date_modified, :items, :type, :bank_payment_id
 
     # @!attribute [r] date_created
-    # @return [DateTime] The tax point of the invoice
+    # Required. The tax point of the invoice.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] date_due
+    # Required. The date the invoice is due.
+    # Use either this field or creditTerms.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] date_accrual
+    # Optional. The invoice accrual date.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] credit_terms
+    # Required. The number of days after the tax point that the invoice is due.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] description
+    # Optional.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] entity_id
+    # Required. The customer or supplier id.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] reference
+    # Optional. A reference string for the invoice.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] project
+    # Optional. The id of the project to assign the invoice to.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] type
+    # Optional. A string identifying the type of the invoice.
+    # Value one of: purchases, sales, cn-sales, cn-purchases.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] bank_payment_id
+    # Optional. The bank account code.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
 
     def initialize data
-      @id = data.savon(:invoice_id).to_i
       @date_created = parse_date data.savon(:date_created)
       @date_due = parse_date data.savon(:date_due)
       @credit_terms = data.savon(:credit_terms).to_i
@@ -24,6 +62,7 @@ module Clearbooks
       @statement_page = data.savon :statement_page
       @date_modified = parse_date data.savon(:date_modified)
       @type = data.savon(:type)
+      @bank_payment_id = data.savon(:bank_payment_id).to_i
 
       @items = Item.build( data[:items].is_a?(Array) ? data[:items] : data[:items][:item])
     end
--- lib/clearbooks/model/item.rb
+++ lib/clearbooks/model/item.rb
@@ -6,6 +6,30 @@ module Clearbooks
   class Item < Base
     attr_reader :description, :unit_price, :quantity, :type, :vat, :vat_rate
 
+    # @!attribute [r] description
+    # Required.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] unit_price
+    # Required. The unit price of an item in pounds.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] quantity
+    # Required.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] type
+    # required. The account code identifying the revenue stream.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] vat
+    # required. The total amount of VAT in pounds. Use either this field or vatRate..
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
+    # @!attribute [r] vat_rate
+    # required. The percentage VAT as a decimal number. Use either this field or vat.
+    # @see https://www.clearbooks.co.uk/support/api/docs/soap/createinvoice/
+
     def initialize data
       @description = data.savon :description
       @unit_price = BigDecimal.new data.savon :unit_price
