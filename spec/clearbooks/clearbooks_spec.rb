a16c3d53e5d7d93ff922fd9b0d0ac8700d176cd2
Oleg Kukareka <oleg@kukareka.com>
2015-04-29 01:02:39 +0300

    More tests on Clearbooks.list_invoices

--- spec/clearbooks/clearbooks_spec.rb
+++ spec/clearbooks/clearbooks_spec.rb
@@ -41,10 +41,24 @@ module Clearbooks
       describe Invoice do
         let(:invoice) { invoices.last }
 
-        it 'has date_created' do
-          expect(invoice.date_created.year).to eq 2015
-          expect(invoice.date_created.month).to eq 4
-          expect(invoice.date_created.day).to eq 27
+        it 'is an Invoice' do
+          expect(invoice).to be_an Invoice
+        end
+
+        it 'has proper attribute values' do
+          expect(invoice.entity_id).to eq 1
+          expect(invoice.invoice_id).to eq 3
+          expect(invoice.invoice_prefix).to eq 'INV'
+          expect(invoice.invoice_number).to eq '3'
+          expect(invoice.reference).to eq 'ref1'
+          expect(invoice.date_created).to eq Date.parse '2015-04-27 00:00:00'
+          expect(invoice.date_due).to eq Date.parse '2015-05-27 00:00:00'
+          expect(invoice.date_accrual).to eq Date.parse '2015-04-27 00:00:00'
+          expect(invoice.credit_terms).to eq 30
+          expect(invoice.project).to eq 7
+          expect(invoice.status).to eq 'approved'
+          expect(invoice.statement_page).to eq 'https://secure.clearbooks.co.uk/accounting/sales/invoice_html/?source=statement'
+          expect(invoice.type).to eq 'S'
         end
 
         it 'has items' do
@@ -54,8 +68,21 @@ module Clearbooks
 
         describe Item do
           let (:item) { invoice.items.last }
-          it 'has description' do
+
+          it 'is an Item' do
+            expect(item).to be_an Item
+          end
+
+          it 'has proper description' do
             expect(item.description).to eq '232323'
+          end
+
+          it 'has proper attribute values' do
+            expect(item.unit_price).to eq 5
+            expect(item.vat).to eq '2'
+            expect(item.quantity).to eq 1
+            expect(item.type).to eq '1001001'
+            expect(item.vat_rate).to eq '0.00:Out'
           end
         end
       end
