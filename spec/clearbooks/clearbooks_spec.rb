467ec85ad22344c11c0a666b4ffa46c3325f8a63
Bjoern Rennhak <bjoern@clothesnetwork.com>
2015-04-29 18:58:04 +0100

    Shabang's in case we want to execute them locally later

--- spec/clearbooks/clearbooks_spec.rb
+++ spec/clearbooks/clearbooks_spec.rb
@@ -1,13 +1,23 @@
-require 'spec_helper'
+#!/usr/bin/env ruby
+
+# System include
 require 'savon'
 
+# Custom include
+require 'spec_helper'
+
+
 module Clearbooks
+
   describe Clearbooks do
+
     describe :config do
       it 'has default WSDL url' do
         expect(Clearbooks.config.wsdl).to eq 'https://secure.clearbooks.co.uk/api/wsdl/'
       end
     end
-  end
-end
+
+  end # of describe Clearbooks
+
+end # of module Clearbooks
 
