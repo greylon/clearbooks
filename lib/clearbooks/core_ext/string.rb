61662b1c3a0d6098e3bf6ac0f0a585a4e5bb7dfb
Bjoern Rennhak <bjoern@clothesnetwork.com>
2015-04-29 17:53:30 +0100

    Filled in missing @brief for string.rb

--- lib/clearbooks/core_ext/string.rb
+++ lib/clearbooks/core_ext/string.rb
@@ -2,17 +2,20 @@
 
 
 # @class        class String
-# @brief
+# @brief        Ruby core String handling
 class String
+
   TRUE_VALUES = %W{1 t true on y yes}.freeze
 
   # @fn         def to_b {{{
   # @brief      Converts String to Boolean value
+  #
   # @credit     https://github.com/prodis/wannabe_bool/blob/master/lib/wannabe_bool/object.rb
   def to_b
     TRUE_VALUES.include?(self.to_s.strip.downcase)
-  end
-end
+  end # }}}
+
+end # of class String
 
 
 # vim:ts=2:tw=100:wm=100
