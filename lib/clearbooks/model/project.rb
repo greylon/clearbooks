a56e21cbe8ffa170430bd2df4ce41a5a7f8c90e6
Bjoern Rennhak <bjoern@clothesnetwork.com>
2015-04-29 17:48:34 +0100

    Visual sugar for project.rb and comments refactoring

--- lib/clearbooks/model/project.rb
+++ lib/clearbooks/model/project.rb
@@ -1,7 +1,16 @@
+#!/usr/bin/env ruby
+
+
+# @module     Clearbooks
+# @brief      Handles Ruby idomatic expression of Clear Books SOAP API
 module Clearbooks
-  # @class Clearbooks Project model {{{
-  # @see https://www.clearbooks.co.uk/support/api/docs/soap/createproject/
+
+  # @class    Clearbooks Project model
+  # @brief    FIXME
+  #
+  # @see      https://www.clearbooks.co.uk/support/api/docs/soap/createproject/
   class Project < Base
+
     attr_reader :id, :description, :project_name, :status
 
     # @!attribute [r] description
@@ -16,22 +25,34 @@ module Clearbooks
     # Optional. String identifying the project status. Use one of the following values: "open", "closed" or "deleted".
     # @see https://www.clearbooks.co.uk/support/api/docs/soap/createproject/
 
+    # @fn       def initialize data {{{
+    # @brief    Constructor for Project model
+    #
+    # @param    [FIXME]     data      FIXME
     def initialize data
-      @id = data.savon(:id).to_i
-      @description = data.savon :description
+      @id           = data.savon(:id).to_i
+      @description  = data.savon :description
       @project_name = data.savon :project_name
-      @status = data.savon :status
-    end
+      @status       = data.savon :status
+    end # }}}
 
+    # @fn       def to_savon {{{
+    # @brief    Converts given Project (self) to savon readable format
+    #
+    # @return   [Hash]      Returns self as Savon readable Hash
     def to_savon
       {
-          project: {
-            :@projectName => @project_name,
-            :@status => @status,
-            :@description => @description
-          }
+        project: {
+          :@projectName => @project_name,
+          :@status => @status,
+          :@description => @description
+        }
       }
-    end
-  end # }}}
-end
+    end # }}}
 
+
+  end # of class Project
+
+end # of module Clearbooks
+
+# vim:ts=2:tw=100:wm=100:syntax=ruby
