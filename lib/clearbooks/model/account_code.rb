87daab08da5b17d01042360b2e78337adc9023d1
Bjoern Rennhak <bjoern@clothesnetwork.com>
2015-04-29 17:52:33 +0100

    Trivial visual sugar for account_code.rb

--- lib/clearbooks/model/account_code.rb
+++ lib/clearbooks/model/account_code.rb
@@ -48,12 +48,14 @@ module Clearbooks
     #
     # @param    [FIXME]     data      FIXME
     def initialize data
-      @id = data.savon(:id).to_i
-      @account_name = data.savon :account_name
-      @group_name = data.savon :group_name
+      @id               = data.savon(:id).to_i
+      @account_name     = data.savon :account_name
+      @group_name       = data.savon :group_name
+
       @default_vat_rate = data.savon :default_vat_rate
-      @show_sales = data.savon(:show_sales).to_b
-      @show_purchases = data.savon(:show_purchases).to_b
+
+      @show_sales       = data.savon(:show_sales).to_b
+      @show_purchases   = data.savon(:show_purchases).to_b
     end # }}}
 
   end # of class AccountCode
